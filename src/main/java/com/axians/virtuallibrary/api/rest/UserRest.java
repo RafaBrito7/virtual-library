9981c12a62c9f00b0e431eaced7e4c4f403eb1e8
RafaBrito7 <rafabrito.office@gmail.com>
2022-06-08 20:51:37 -0300

    Some documentations of UserRest

--- src/main/java/com/axians/virtuallibrary/api/rest/UserRest.java
+++ src/main/java/com/axians/virtuallibrary/api/rest/UserRest.java
@@ -2,8 +2,6 @@ package com.axians.virtuallibrary.api.rest;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,11 +20,9 @@ import com.axians.virtuallibrary.api.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/user")
-@Tag(name = "User", description = "User Operations")
 @CrossOrigin("*")
 @PreAuthorize("hasRole('ADMIN')")
 public class UserRest {
@@ -38,21 +34,21 @@ public class UserRest {
 	}
 	
 	@PostMapping("/create")
-	@ApiOperation("Operation to create a User, returns status 200 if performed")
+	@ApiOperation("Operation to create a user for system(admin users only)")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "OK"),
+			@ApiResponse(responseCode = "200", description = "Return a status of created"),
 			@ApiResponse(responseCode = "400", description = "Bad Request, some parameter invalid"),
 			@ApiResponse(responseCode = "403", description = "Forbidden, not authorized to create"),
-			@ApiResponse(responseCode = "409", description = "Conflict with Other Equals Entity"),
+			@ApiResponse(responseCode = "409", description = "Conflict, there is already a user created with the parameters passed"),
 			@ApiResponse(responseCode = "500", description = "Internal Server Error"),
 			@ApiResponse(responseCode = "503", description = "Service Unavailable")
 	})
-	public ResponseEntity<?> create(@Valid @RequestBody UserDTO user) {
+	public ResponseEntity<?> create(@RequestBody UserDTO user) {
 		this.userService.create(user);
 		return ResponseEntity.ok(new ResponseEntity<>(HttpStatus.CREATED));
 	}
 	
-	@GetMapping("/list")
+	@GetMapping(value =  "/list", produces="application/json", consumes = "application/json")
 	@ApiOperation("Operation to list all users with status actived")
 	public ResponseEntity<?> listAll() {
 		List<UserDTO> users = this.userService.listAll();
